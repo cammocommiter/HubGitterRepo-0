623ada79c462c02a19197ab012ed44865510391e
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-09-29 11:58:08 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,16 +1,5 @@
-namespace :thing do
-  desc "it does a thing"
-  task :work, [:option, :foo, :bar] do |task, args|
-    puts "work", args
-  end
-  
-  task :another, [:option, :foo, :bar] do |task, args|
-    puts "another #{args}"
-    Rake::Task["thing:work"].invoke(args[:option], args[:foo], args[:bar])
-    # or splat the args
-    # Rake::Task["thing:work"].invoke(*args)
-  end
+puts Benchmark.measure("require #{file}") {
+  Kernel.require file
+}.format("%n: %t %r")
 
-end
-
-StandardError
+disable-binary
