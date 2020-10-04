a7cb89bc75633fd0e32b4f15d12931d01913123e
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-10-04 18:27:09 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,2 +1,10 @@
-/opt/local
-#==
+elsif !reject_new_record?(association_name, attributes)
+  method = "build_#{association_name}"
+  if respond_to?(method)
+    send(method, attributes.except(*UNASSIGNABLE_KEYS))
+  else
+    raise ArgumentError, "Cannot build association #{association_name}. Are you trying to build a polymorphic one-to-one association?"
+  end
+end
+
+require 'active_support/inflector'
