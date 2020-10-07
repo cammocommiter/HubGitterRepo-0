cff0eb41ca85acec96d3fe2ecb5a3ea0b970143a
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-10-07 11:52:53 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,10 +1,10 @@
-elsif !reject_new_record?(association_name, attributes)
-  method = "build_#{association_name}"
-  if respond_to?(method)
-    send(method, attributes.except(*UNASSIGNABLE_KEYS))
-  else
-    raise ArgumentError, "Cannot build association #{association_name}. Are you trying to build a polymorphic one-to-one association?"
-  end
-end
+development:
+  adapter: postgresql
+  encoding: unicode
+  database: blog_development
+  pool: 5
+  username: blog
+  password:
+  host: localhost
 
-require 'active_support/inflector'
+ReactDOM
