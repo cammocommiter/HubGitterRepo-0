10f99e89644bfe590c5da47528cbcde95f30a3b3
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-10-07 14:28:59 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,16 +1,2 @@
-# use mongoid
-class User
-  include Mongoid::Document
-end
-
-
-# then
-user = User.where(email: 'joe@example.com').first
-
-if user
-  user.password = new_password
-  user.password_confirmation = new_password
-  user.save
-end
-
-nil
+/bin/sh
+child_process.exec
