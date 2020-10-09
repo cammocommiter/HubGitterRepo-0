66af8122d62ed9aaea3c6488d2a8c179908887d7
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-10-09 10:33:10 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,2 +1,28 @@
-a in [1,2]
-z.round(2)
+before_action :fetch_current_category, only: [:show,:edit,:update] 
+before_action :fetch_categories, only: [:show,:edit,:update] 
+before_action :fetch_search_results, only: [:show,:edit,:update] #or whatever
+
+def show
+  rate
+end
+
+private
+
+def fetch_current_category
+  @category = Category.friendly.find(params[:id])
+end
+
+def fetch_categories
+  @categories = Category.all
+end
+
+def fetch_search_results
+  @search = category.products.approved.order(updated_at: :desc).ransack(params[:q])
+  @products = @search.result.page(params[:page]).per(50)
+end
+
+opts = Trollop::options do
+  opt :quiet, "Use minimal output", :short => 'q'
+  opt :interactive, "Be interactive"
+  opt :filename, "File to process", :type => String
+end
