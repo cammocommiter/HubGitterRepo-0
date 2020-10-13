7fec15b8d3ccd22fb0466a9c4d8ccb64e3696837
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-10-13 14:00:52 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,7 +1,2 @@
-word = "Now is the,time for'all good people"
-word.split(/[\s,']/)
- => ["Now", "is", "the", "time", "for", "all", "good", "people"] 
-
-if __FILE__ == $0
-  # this library may be run as a standalone script
-end
+sudo port install curl-ca-bundle
+change_column :users, :admin, :boolean, :default => false
