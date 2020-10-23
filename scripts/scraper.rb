027ac6b890a042c029644a852d9d3b65992869e7
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-10-24 07:31:17 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,5 +1,14 @@
-scaffold
-class Person
-  attr_accessor :age
-  ...
+form do |f|
+    f.inputs "Member Details" do
+        f.input :first_name
+        f.input :last_name
+        f.input :email
+        if !f.object.new_record?
+            f.input :password
+            f.input :password_confirmation
+        end
+    end
+    f.button :Submit
 end
+
+trained
