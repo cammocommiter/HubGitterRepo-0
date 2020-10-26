51502e6b665953426b11aa83ddf762699a5384a1
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-10-26 19:21:23 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,14 +1,2 @@
-form do |f|
-    f.inputs "Member Details" do
-        f.input :first_name
-        f.input :last_name
-        f.input :email
-        if !f.object.new_record?
-            f.input :password
-            f.input :password_confirmation
-        end
-    end
-    f.button :Submit
-end
-
-trained
+/generators/clearance_features/templates/features/
+gem 'unf' 
