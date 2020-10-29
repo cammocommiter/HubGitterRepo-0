a3524744088177cd1a5d295cab01fecdd6a66ec4
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-10-30 07:55:22 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,12 +1,14 @@
-#1 query
-Dog.find(:all).each do |dog|
-   #N queries
-   dog.owner.siblings.each do |sibling|
-      #N queries per above N query!!
-      sibling.pets.each do |pet|
-         #Do something here
-      end
-   end
-end
+>> foo = [1, 2, 3]
+=> [1, 2, 3]
+>> goo = [2, 3, 4]
+=> [2, 3, 4]
+>> foo - goo
+=> [1]
 
-elsif
+# Only run the following code when this file is the main file being run
+# instead of having been required or loaded by another file
+if __FILE__==$0
+  # Find the parent directory of this file and add it to the front
+  # of the list of locations to look in when using require
+  $:.unshift File.expand_path("../../", __FILE__)  
+end
