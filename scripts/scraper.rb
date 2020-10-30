29ff07ab84012a5538eaa83895a09f9707fde1ea
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-10-31 07:55:36 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,14 +1,8 @@
->> foo = [1, 2, 3]
-=> [1, 2, 3]
->> goo = [2, 3, 4]
-=> [2, 3, 4]
->> foo - goo
-=> [1]
+string[N..-1]
 
-# Only run the following code when this file is the main file being run
-# instead of having been required or loaded by another file
-if __FILE__==$0
-  # Find the parent directory of this file and add it to the front
-  # of the list of locations to look in when using require
-  $:.unshift File.expand_path("../../", __FILE__)  
-end
+respond_to do |format|
+
+  format.html # show.html.erb
+  format.json { render json: @user }
+
+ end
