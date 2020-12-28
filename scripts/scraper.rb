6c821510d862098b993786604220e85c0391ca4c
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-12-29 07:43:23 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,10 +1,2 @@
-brew update
-brew install ruby
-
-# If you use bash
-echo 'export PATH=/usr/local/Cellar/ruby/2.4.1_1/bin:$PATH' >> ~/.bash_profile 
-
-# If you use ZSH:
-echo 'export PATH=/usr/local/Cellar/ruby/2.4.1_1/bin:$PATH' >> ~/.zprofile
-
-&
+File.expand_path
+$ which -a ruby
