bbea8f150ad58f75bbf41dfdd5f1657784f28b6f
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-12-31 07:33:36 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,2 +1,19 @@
-File.expand_path
-$ which -a ruby
+describe Array do
+  describe "with 3 items" do
+    before { @arr = [1, 2, 3] }
+
+    specify { @arr.should_not be_empty }
+    specify { @arr.count.should eq(3) }
+  end
+end
+
+describe Array do
+  describe "with 3 items" do
+    subject { [1, 2, 3] }
+
+    it { should_not be_empty }
+    its(:count) { should eq(3) }
+  end
+end
+
+a.map {|x| x[/\d+/]}
