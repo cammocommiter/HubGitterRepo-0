3188e4aeeea0800c09a436523f84d9180bd8dc79
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2021-01-26 07:14:03 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,3 +1,9 @@
-bundle clean
-"String".start_with? "S" # true
-"String".end_with? "4" # false
+GoogleChart::PieChart.new('320x200', "Things I Like To Eat", false) do |pc| 
+  pc.data "Broccoli", 30
+  pc.data "Pizza", 20
+  pc.data "PB&J", 40 
+  pc.data "Turnips", 10 
+  puts pc.to_url 
+end
+
+on
