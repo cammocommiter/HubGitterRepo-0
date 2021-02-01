5ae8b3f389ce09f02182a888d4b73c3caed453a0
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2021-02-02 04:37:27 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,9 +1,9 @@
-GoogleChart::PieChart.new('320x200', "Things I Like To Eat", false) do |pc| 
-  pc.data "Broccoli", 30
-  pc.data "Pizza", 20
-  pc.data "PB&J", 40 
-  pc.data "Turnips", 10 
-  puts pc.to_url 
-end
+str = 'this is a multi-line string'\
+  ' using implicit concatenation'\
+  ' to prevent spare \n\'s'
 
-on
+=> "this is a multi-line string using implicit concatenation to eliminate spare
+\\n's"
+
+array = [1, 2, 3, 4, 5, 6]
+array.each { |x| puts x }
