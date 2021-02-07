f3be3227ee7cc3d39864e1abd413e1c3048112f1
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2021-02-08 07:14:48 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,9 +1,9 @@
-str = 'this is a multi-line string'\
-  ' using implicit concatenation'\
-  ' to prevent spare \n\'s'
+all : usera userb userc....
 
-=> "this is a multi-line string using implicit concatenation to eliminate spare
-\\n's"
+usera:
+       imapsync usera
+userb:
+       imapsync userb
+....
 
-array = [1, 2, 3, 4, 5, 6]
-array.each { |x| puts x }
+cannot find -lgmp
