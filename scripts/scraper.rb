edbab3b73a98fa83ce02255b01be4095cc2ae18c
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2021-02-09 07:51:03 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,9 +1,33 @@
-all : usera userb userc....
+#!/usr/bin/ruby1.8
 
-usera:
-       imapsync usera
-userb:
-       imapsync userb
-....
+require 'pp'
+mooth = [
+  "booth", "month", "mooch", "morth",
+  "mouth", "mowth", "sooth", "tooth"
+]
+PP.pp(mooth, $>, 40)
+# => ["booth",
+# =>  "month",
+# =>  "mooch",
+# =>  "morth",
+# =>  "mouth",
+# =>  "mowth",
+# =>  "sooth",
+# =>  "tooth"]
+PP.pp(mooth, $>, 79)
+# => ["booth", "month", "mooch", "morth", "mouth", "mowth", "sooth", "tooth"]
 
-cannot find -lgmp
+class Api::V1::SessionsController < Devise::SessionsController  
+  def create  
+    respond_to do |format|  
+      format.html { super }  
+      format.json {  
+        warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#new")  
+        render :status => 200, :json => { :error => "Success" }  
+      }  
+    end  
+  end  
+  def destroy  
+    super  
+  end  
+end  
