f196eaf13600e21ce4c27457df804aa9de6e044c
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2021-06-02 17:02:27 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,2 +1,11 @@
-(1..10).include?(number)
-fail
+class Array
+  def same_values?
+    self.uniq.length == 1
+  end
+end
+
+
+[1, 1, 1, 1].same_values?
+[1, 2, 3, 4].same_values?
+
+git://github.com/Dakuan/client_side_validations.git
