106757d068091b70e099458e668ff895a4f1b029
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2021-09-07 17:05:51 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,5 +1,2 @@
-bundle exec rake db:drop RAILS_ENV=test
-bundle exec rake db:create RAILS_ENV=test
-bundle exec rake db:schema:load RAILS_ENV=test
-
-has_many :memberships, dependent: :delete_all
+self.class.whatever
+ActionController::Base
