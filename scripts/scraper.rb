6d91f8d144c5671c9dda26c38b885ad7a30aa1f3
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2021-10-08 10:37:25 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,2 +1,10 @@
-application_controller.rb
-0.0.0.0
+<%= javascript_include_tag "application" %>
+<%= yield :javascript_includes %>
+
+GET    /items        #=> index
+GET    /items/1      #=> show
+GET    /items/new    #=> new
+GET    /items/1/edit #=> edit
+PUT    /items/1      #=> update
+POST   /items        #=> create
+DELETE /items/1      #=> destroy
